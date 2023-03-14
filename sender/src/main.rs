c705f4545e4f6b2cb7ec2dd21f0aa3bdbb051c2c
Daniyar Itegulov <ditegulov@gmail.com>
2023-03-14 18:54:21 +1100

    refactor sender

--- sender/src/main.rs
+++ sender/src/main.rs
@@ -1,42 +1,44 @@
-use std::{env, time::Duration};
-
-use near_crypto::SecretKey;
-use near_jsonrpc_client::{methods, JsonRpcClient};
-use near_jsonrpc_primitives::types::query::QueryResponseKind;
-use near_primitives::{
-    transaction::{Action, FunctionCallAction, Transaction},
-    types::{AccountId, BlockReference},
-};
-
+use imap::Session;
+use native_tls::TlsStream;
 use serde_json::json;
+use std::str::FromStr;
+use std::{env, net::TcpStream, time::Duration};
+use workspaces::{types::SecretKey, Network};
+use workspaces::{AccountId, InMemorySigner, Worker};
 
-extern crate imap;
-extern crate native_tls;
+fn create_imap_session(
+    username: &str,
+    password: &str,
+) -> anyhow::Result<Session<TlsStream<TcpStream>>> {
+    let domain = "imap.gmail.com";
+    let tls = native_tls::TlsConnector::builder().build()?;
+
+    // we pass in the domain twice to check that the server's TLS
+    // certificate is valid for the domain we're connecting to.
+    let client = imap::connect((domain, 993), domain, &tls)?;
+
+    // the client we have here is unauthenticated.
+    // to do anything useful with the e-mails, we need to log in
+    Ok(client.login(username, password).map_err(|e| e.0)?)
+}
 
 fn fetch_inbox_from(
     min_value: u32,
     username: &str,
     password: &str,
-) -> imap::error::Result<(u32, Vec<String>)> {
-    let domain = "imap.gmail.com";
-    let tls = native_tls::TlsConnector::builder().build().unwrap();
+) -> anyhow::Result<(u32, Vec<String>)> {
+    // Ensure that sessions are short-lived to avoid timeouts
+    let mut imap_session = create_imap_session(username, password)?;
 
-    // we pass in the domain twice to check that the server's TLS
-    // certificate is valid for the domain we're connecting to.
-    let client = imap::connect((domain, 993), domain, &tls).unwrap();
-
-    // the client we have here is unauthenticated.
-    // to do anything useful with the e-mails, we need to log in
-    let mut imap_session = client.login(username, password).map_err(|e| e.0)?;
-
-    // we want to fetch the first email in the INBOX mailbox
-    let mailbox = imap_session.select("INBOX")?;
+    // Opens mailbox in read-only mode.
+    // TODO: Make sure that INBOX is the correct name for our email provider of choosing.
+    let mailbox = imap_session.examine("INBOX")?;
 
     let result = if mailbox.exists > min_value {
         let max_value = mailbox.exists;
-        let foo = (min_value + 1..=max_value).map(|x| format!("{}", x));
+        let mail_indices = (min_value + 1..=max_value).map(|x| x.to_string());
 
-        let messages = imap_session.fetch(itertools::join(foo, ","), "RFC822")?;
+        let messages = imap_session.fetch(itertools::join(mail_indices, ","), "RFC822")?;
 
         Ok((
             max_value,
@@ -58,97 +60,57 @@ fn fetch_inbox_from(
     result
 }
 
-async fn send_mail_to_near(
-    mail: String,
-    signer_account_id: &AccountId,
-    signer_secret_key: &SecretKey,
-    nonce: Option<u64>,
-) -> Result<u64, Box<dyn std::error::Error>> {
-    let client = JsonRpcClient::connect("http://localhost:3030");
-
-    let signer = near_crypto::InMemorySigner::from_secret_key(
-        signer_account_id.clone(),
-        signer_secret_key.clone(),
-    );
-
-    let access_key_query_response = client
-        .call(methods::query::RpcQueryRequest {
-            block_reference: BlockReference::latest(),
-            request: near_primitives::views::QueryRequest::ViewAccessKey {
-                account_id: signer.account_id.clone(),
-                public_key: signer.public_key.clone(),
-            },
-        })
-        .await?;
-    let current_nonce = match access_key_query_response.kind {
-        QueryResponseKind::AccessKey(access_key) => access_key.nonce,
-        _ => Err("failed to extract current nonce")?,
-    };
-
-    let nonce = if let Some(nonce) = nonce {
-        std::cmp::max(nonce, current_nonce)
-    } else {
-        current_nonce
-    };
-
-    println!("Will use nonce {}", nonce + 1);
-
+async fn send_mail<N: Network + ?Sized>(
+    worker: &Worker<N>,
+    signer: &InMemorySigner,
+    controller_account_id: &AccountId,
+    mail: &str,
+) -> anyhow::Result<()> {
     let payload = mail.as_bytes().to_vec();
+    worker
+        .call(signer, controller_account_id, "receive_email")
+        .args(json!({ "full_email": payload }).to_string().into_bytes())
+        .max_gas()
+        .transact()
+        .await?
+        .into_result()?;
+    Ok(())
+}
 
-    let transaction = Transaction {
-        signer_id: signer.account_id.clone(),
-        public_key: signer.public_key.clone(),
-        nonce: nonce + 1,
-        receiver_id: signer_account_id.clone(),
-        block_hash: access_key_query_response.block_hash,
-        actions: vec![Action::FunctionCall(FunctionCallAction {
-            method_name: "receive_email".to_string(),
-            args: json!({ "full_email": payload }).to_string().into_bytes(),
-            gas: 300_000_000_000_000, // 100 TeraGas
-            deposit: 0,
-        })],
-    };
-
-    let request = methods::broadcast_tx_async::RpcBroadcastTxAsyncRequest {
-        signed_transaction: transaction.sign(&signer),
-    };
-
-    let tx_hash = client.call(request).await?;
-
-    println!("Sent request {}", tx_hash);
-
-    Ok(nonce + 1)
+fn parse_env_var<T: FromStr>(key: &str) -> anyhow::Result<T> {
+    Ok(env::var(key)
+        .map_err(|err| anyhow::anyhow!("Failed to get {}: {}", key, err))?
+        .parse()
+        .map_err(|_| anyhow::anyhow!("Failed to parse {}", key))?)
 }
 
 #[tokio::main]
-
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let near_account: AccountId = env::var("SENDER_NEAR_ACCOUNT")?.parse().unwrap();
-    let near_account_secret_key: SecretKey = env::var("SENDER_SECRET_KEY")?.parse().unwrap();
-    let imap_username = env::var("SENDER_IMAP_USERNAME")?;
-    let imap_password = env::var("SENDER_IMAP_PASSWORD")?;
+    let signer_account: AccountId = parse_env_var("SENDER_SIGNER_NEAR_ACCOUNT")?;
+    let signer_account_secret_key: SecretKey = parse_env_var("SENDER_SIGNER_SECRET_KEY")?;
+    let controller_account: AccountId = parse_env_var("SENDER_CONTROLLER_NEAR_ACCOUNT")?;
+    let imap_username: String = parse_env_var("SENDER_IMAP_USERNAME")?;
+    let imap_password: String = parse_env_var("SENDER_IMAP_PASSWORD")?;
+
+    // TODO: Make network dynamically choosable based on SENDER_NEAR_NETWORK
+    let worker = workspaces::testnet().await?;
+    let signer = InMemorySigner::from_secret_key(signer_account, signer_account_secret_key);
 
     println!("Starting...");
 
-    // CHANGE.
+    // TODO: Properly detect unprocessed emails. See https://github.com/near/email-auth/issues/4
     let mut min_value = 0;
-    let (value, _) =
-        fetch_inbox_from(min_value, imap_username.as_str(), imap_password.as_str()).unwrap();
+    let (value, _) = fetch_inbox_from(min_value, &imap_username, &imap_password)?;
     min_value = value;
     println!("Already {} email present - ignoring.", min_value);
 
     loop {
-        let (value, mails) =
-            fetch_inbox_from(min_value, imap_username.as_str(), imap_password.as_str()).unwrap();
+        let (value, mails) = fetch_inbox_from(min_value, &imap_username, &imap_password)?;
         min_value = value;
-        let mut nonce = None;
-        if mails.len() > 0 {
+        if !mails.is_empty() {
             println!("Got new mail: {:?}", mails.len());
             for mail in mails.iter() {
-                let result =
-                    send_mail_to_near(mail.clone(), &near_account, &near_account_secret_key, nonce)
-                        .await?;
-                nonce = Some(result);
+                send_mail(&worker, &signer, &controller_account, mail).await?;
             }
         }
         tokio::time::sleep(Duration::from_secs(10)).await;
